8fea6d39e0659a36a5534b5a72b49ac13ee7df7e
fire.anton <fire.anton@gmail.com>
2026-08-27 22:56:40 +0300

    update to vertion 1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         minSdk = 23
         targetSdk = 37
 
-        versionCode = 6
-        versionName = "1.6"
+        versionCode = 7
+        versionName = "1.7"
     }
 
     buildFeatures {
